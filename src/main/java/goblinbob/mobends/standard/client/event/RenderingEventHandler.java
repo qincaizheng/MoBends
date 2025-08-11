1398f7a3964cd53de5d453041609e8bf6614bc27
qdd <83817823+qincaizheng@users.noreply.github.com>
2025-08-11 16:35:22 +0800

    Update RenderingEventHandler.java

--- src/main/java/goblinbob/mobends/standard/client/event/RenderingEventHandler.java
+++ src/main/java/goblinbob/mobends/standard/client/event/RenderingEventHandler.java
@@ -25,8 +25,6 @@ public class RenderingEventHandler
 
         if (!BenderHelper.isEntityAnimated(player))
         	return;
-        if (player.isSprinting())
-            return;
 
         RenderPlayer renderPlayer = (RenderPlayer) mc.getRenderManager().<AbstractClientPlayer>getEntityRenderObject(player);
         PlayerMutator mutator = (PlayerMutator) BenderHelper.getMutatorForRenderer(AbstractClientPlayer.class, renderPlayer);
